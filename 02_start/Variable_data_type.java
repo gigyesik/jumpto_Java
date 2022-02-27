78bba8e18053875f7e39423ccd3e6ae222ea77bb
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-02-27 13:57:15 +0900

    Add files via upload

---
+++ 02_start/Variable_data_type.java
@@ -0,0 +1,10 @@
+public class Variable_data_type {
+    public static void main(String[] args) {
+        int a;
+        String b;
+        a = 1;
+        b = "hello java";
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
