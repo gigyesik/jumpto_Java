c7fb6aacebb948704453f72e6b809cbec4ddee75
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-02-27 15:01:13 +0900

    Add files via upload

---
+++ 03_variable_and_datatype/Datatype_char.java
@@ -0,0 +1,11 @@
+public class Datatype_char {
+    public static void main(String[] args) {
+        char a1 = 'a'; // string
+        char a2 = 97; // ascii
+        char a3 = '\u0061'; // unicode
+
+        System.out.println(a1); // a
+        System.out.println(a2); // a
+        System.out.println(a3); // a
+    }
+}
