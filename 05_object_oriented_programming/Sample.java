cdc87464ee564accd942af974f200c54a22e1332
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-03-04 18:07:54 +0900

    Create Sample.java

---
+++ 05_object_oriented_programming/Sample.java
@@ -0,0 +1,35 @@
+class Calculator {
+    int result = 0;
+    
+    int add(int num) {    
+        result += num;
+        return result;
+    }
+
+    int sub(int num) {
+        result -= num;
+        return result;
+    }
+}
+/*
+class Calculator2 {
+    static int result = 0;
+    
+    static int add(int num) {    
+        result += num;
+        return result;
+    }
+}
+*/
+public class Sample {
+    public static void main(String[] args) {
+        Calculator cal1 = new Calculator();
+        Calculator cal2 = new Calculator();
+
+        System.out.println(cal1.add(3));
+        System.out.println(cal1.add(4));
+
+        System.out.println(cal2.add(3));
+        System.out.println(cal2.add(7));
+    }
+}
