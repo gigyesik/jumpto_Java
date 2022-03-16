5ceaf536c6a94e234247ea8ca460283d031f7328
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-03-16 14:38:00 +0900

    Add files via upload

---
+++ 05_object_oriented_programming/practice_Q1.java
@@ -0,0 +1,32 @@
+// 05 Practice Q1
+
+class Calculator {
+    int value;
+
+    Calculator() {
+        this.value = 0;
+    }
+
+    void add(int val) {
+        this.value += val;
+    }
+
+    int getValue() {
+        return this.value;
+    }
+}
+
+class UpgradeCalculator extends Calculator {
+    void minus(int val) {
+        this.value -= val;
+    }
+}
+
+public class practice_Q1 {
+    public static void main(String[] args) {
+        UpgradeCalculator cal = new UpgradeCalculator();
+        cal.add(10);
+        cal.minus(3);
+        System.out.println(cal.getValue());  // 7
+    }
+}
