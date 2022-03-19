f593df25ea95205dba69fee2b07f7ea4dedae2c7
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-03-19 17:17:24 +0900

    Add files via upload

---
+++ 06_input_output/practice_Q1.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class practice_Q1 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+    }
+}
