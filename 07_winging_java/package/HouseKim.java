010e51168b35b5de236b0ab55d3d00047abd52ae
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-03-22 15:26:34 +0900

    Add files via upload

--- 07_winging_java/package/HouseKim.java
+++ 07_winging_java/package/HouseKim.java
@@ -1,4 +1,5 @@
 package house;
 
 public class HouseKim {
+    String lastname = "kim"; // default
 }
--- 07_winging_java/package/HousePark.java
+++ 07_winging_java/package/HousePark.java
@@ -1,4 +1,17 @@
 package house;
 
 public class HousePark {
+    /*
+    // default
+    String lastname = "park";
+
+    public static void main(String[] args) {
+        HouseKim kim = new HouseKim();
+        System.out.println(kim.lastname); // kim
+    }
+     */
+
+    protected String lastname = "park";
+    // public
+    public String info = "this is public message";
 }
