fe9ce0c84774e5eb21577a22b1d765c4d6e0e0bb
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-03-22 15:26:10 +0900

    Add files via upload

---
+++ 07_winging_java/Access_Modifier.java
@@ -0,0 +1,13 @@
+public class Access_Modifier {
+    // private
+    private String secret;
+    private String getSecret() {
+        return this.secret;
+    }
+
+    // default
+
+    // protected
+
+    // public
+}
