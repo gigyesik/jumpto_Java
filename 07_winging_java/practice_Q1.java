c1ed19942689d9f1e53f5af630286a2600ee210d
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-03-24 14:31:21 +0900

    Add files via upload

---
+++ 07_winging_java/practice_Q1.java
@@ -0,0 +1,8 @@
+import house1.HousePark1;
+
+public class practice_Q1 {
+    public static void main(String[] args) {
+        HousePark1 housePark1 = new HousePark1();
+        System.out.println(housePark1.getLastname());
+    }
+}
